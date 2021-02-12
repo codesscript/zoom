4b08bfe1a80fcbc28314ca677d4616540e6a161f
unknown <you@example.com>
2021-02-12 18:32:14 +0530

    First commit

---
+++ signature.js
@@ -0,0 +1,28 @@
+const base64JS = require('js-base64');
+const hmacSha256 = require('crypto-js/hmac-sha256');
+const encBase64 = require('crypto-js/enc-base64');
+
+function generateSignature(data) {
+    let signature = '';
+    // Prevent time sync issue between client signature generation and zoom
+    const ts = new Date().getTime() - 30000;
+    const msg = base64JS.Base64.encode(data.apiKey + data.meetingNumber + ts + data.role);
+    const hash = hmacSha256(msg, data.apiSecret);
+    signature = base64JS.Base64.encodeURI(`${data.apiKey}.${data.meetingNumber}.${ts}.${data.role}.${encBase64.stringify(hash)}`);
+    return signature;
+}
+
+const data = {apiKey: "34n5fY-dQAqLjcXZjosfyw" ,
+apiSecret: "B7p3dhFTYrLXdwTfJipACMJxhNBYItDm0vxJ",
+meetingNumber:82292933672,
+role: 0}
+
+console.log(generateSignature(data));
+var s=generateSignature(data);
+
+ var http = require('http');
+    http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(s);
+    }).listen(8000, "127.0.0.1");
+    console.log('Server running at http://127.0.0.1:8000');
